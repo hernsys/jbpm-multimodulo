6702179024f655cf07609f70f55dab7dd6537ccf
Horacio Antar <horacioantar@gmail.com>
2013-10-21 15:36:49 -0300

    initial configuration

---
+++ jbpm-backend/src/main/java/com/hernsys/backend/App.java
@@ -0,0 +1,13 @@
+package com.hernsys.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
