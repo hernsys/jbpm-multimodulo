ffd98418071fc04238ec25f4c0126b2ca2a0a4ab
Horacio Antar <horacioantar@gmail.com>
2013-10-23 11:48:54 -0300

    module mock - entities - cxf(initial configuration)

---
+++ jbpm-services/src/main/java/como/hernsys/services/Step4Service.java
@@ -0,0 +1,31 @@
+package como.hernsys.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.hernsys.entities.Customer;
+
+@Path("/step4Service")
+
+@Produces("application/xml")
+public interface Step4Service {
+
+	
+	@GET
+    @Path("/getCustomerByIdJson/{id}")
+	@Produces("application/json")
+	//http://localhost:8081/cxf/projectRest/step4Service/getCustomerByIdJson/1/
+	public Customer getCustomerByIdJson(@PathParam("id") Long id);
+	
+	@GET
+    @Path("/getCustomerByIdXml/{id}")
+	@Produces("application/xml")
+	//http://localhost:8081/cxf/projectRest/step4Service/getCustomerByIdXml/1/
+	public Customer getCustomerByIdXml(@PathParam("id") Long id);
+	
+	
+	
+	
+}
